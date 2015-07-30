c5acbec22c2ed29e09ce0b4f8ed305de46a4c6fc
Sriram <sriram@indix.com>
2015-07-30 11:11:39 +0530

    DFOC for both old and new MR library

---
+++ hadoop-mr-s3/src/main/java/org/apache/hadoop/mapreduce/lib/DirectFileOutputCommitter.java
@@ -0,0 +1,85 @@
+package org.apache.hadoop.mapreduce.lib;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.s3native.NativeS3FileSystem;
+import org.apache.hadoop.mapreduce.JobContext;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputCommitter;
+
+import java.io.IOException;
+
+public class DirectFileOutputCommitter extends FileOutputCommitter {
+    private static final Log LOG = LogFactory.getLog(DirectFileOutputCommitter.class);
+    private Path outputPath = null;
+    private final boolean directWrite;
+
+    public DirectFileOutputCommitter(Path outputPath, TaskAttemptContext context) throws IOException {
+        super(outputPath, context);
+        if(outputPath != null && context != null) {
+            this.outputPath = outputPath;
+            this.directWrite = this.isDirectWrite(context, outputPath);
+        } else {
+            this.directWrite = false;
+        }
+
+    }
+
+    public void setupJob(JobContext context) throws IOException {
+        if(this.directWrite) {
+            LOG.info("Nothing to setup for DFOC");
+        } else {
+            super.setupJob(context);
+        }
+
+    }
+
+    public void cleanupJob(JobContext context) throws IOException {
+        if(this.directWrite) {
+            LOG.info("Nothing to cleanup for DFOC");
+        } else {
+            super.cleanupJob(context);
+        }
+
+    }
+
+    public void setupTask(TaskAttemptContext context) throws IOException {
+        if(!this.directWrite) {
+            super.setupTask(context);
+        }
+
+    }
+
+    public void commitTask(TaskAttemptContext context) throws IOException {
+        if(this.directWrite) {
+            LOG.info("This is a NOOP for DFOC");
+        } else {
+            super.commitTask(context);
+        }
+
+    }
+
+    /*TODO: Should we cleanup partial files during job abort? */
+    public void abortTask(TaskAttemptContext context) throws IOException {
+        if(this.directWrite) {
+            LOG.info("Nothing to clean up on abort for DFOC");
+        } else {
+            super.abortTask(context);
+        }
+
+    }
+
+    public boolean needsTaskCommit(TaskAttemptContext context) throws IOException {
+        return this.directWrite ? false : super.needsTaskCommit(context);
+    }
+
+    public Path getWorkPath() throws IOException {
+        return this.directWrite ? this.outputPath : super.getWorkPath();
+    }
+
+    private boolean isDirectWrite(JobContext jc, Path outputPath) throws IOException {
+        return outputPath.getFileSystem(jc.getConfiguration()) instanceof NativeS3FileSystem;
+    }
+}
